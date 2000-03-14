e57991678f003eefbc89aa82c7d22760b3b45e9d
Frederic Gobry <fredgo@src.gnome.org>
2000-03-14 16:43:54 +0000

    added Medline config

---
+++ Pyblio/ConfDir/Medline.py
@@ -0,0 +1,21 @@
+from Pyblio import Config
+
+Config.define ('medline/mapping',
+               """ A hash table containing field names correspondances """)
+
+Config.set ('medline/mapping', {
+    'UI' : 'medlineref',
+    'AU' : 'author',
+    'DP' : 'date',
+    'TI' : 'title',
+    'LA' : 'language',
+    'MH' : 'keywords',
+    'AD' : 'affiliation',
+    'AB' : 'abstract',
+    'AD' : 'authoraddress',
+    'TA' : 'journal',
+    'CY' : 'country',
+    'PG' : 'pages',
+    'IP' : 'number',
+    'VI' : 'volume',
+    })
