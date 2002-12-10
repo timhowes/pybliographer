e698eaced3ebd0945cf48960b1aea14d57cff1c2
Frederic Gobry <fredgo@src.gnome.org>
2002-12-10 22:18:23 +0000

    updated db api

--- doc/design/db.py
+++ doc/design/db.py
@@ -7,21 +7,40 @@ class Database (api.Database):
     tables = (
         '''CREATE SEQUENCE record_id''',
         
-        '''CREATE TABLE work (
+        '''CREATE TABLE record (
+             id   int  primary key,
+             type char
+           )''',
+
+        '''CREATE TABLE role (
              id int primary key
            )''',
 
-        '''CREATE TABLE expression (
+        '''CREATE TABLE record_link (
              id int primary key
            )''',
+
+        '''CREATE TABLE type (
+             id int primary key
+           )''',
+
+        '''CREATE TABLE person_t (
+             first  text,
+             middle text,
+             last   text
+           ) INHERITS (type) ''',
         
-        '''CREATE TABLE manifestation (
-             id int primary key
-           )''',
-        
-        '''CREATE TABLE item (
-             id int primary key
-           )''',
+        '''CREATE TABLE text_t (
+             text text,
+             lang text
+           ) INHERITS (type) ''',
+
+        '''CREATE TABLE attribute (
+             index  int,
+             role   int REFERENCES role (id),
+             data   int REFERENCES type (id),
+             record int REFERENCES record (id)
+        )''',
         )
     
     def __init__ (self, uri, create = 0):
@@ -41,45 +60,62 @@ class Database (api.Database):
 
 class Record (object):
 
+    def register (self, db):
+        db._op.execute ("SELECT nextval ('record_id')")
+        self.id = db._op.fetchone () [0]
+        self.db = db
+        db._op.execute ("INSERT INTO record (id, type) values (%s, %s)",
+                        self.id, self._type)
+        db._db.commit ()
+        return
+
+
+class Work (Record, api.Work):
+
+    _type = 'w'
+
+class Expression (Record, api.Expression):
+
+    _type = 'e'
+
+    
+class Manifestation (Record, api.Manifestation):
+
+    _type = 'm'
+
+    
+class Item (Record, api.Item):
+
+    _type = 'i'
+
+
+class Type (object):
+
     def _register (self, db):
         db._op.execute ("SELECT nextval ('record_id')")
         self.id = db._op.fetchone () [0]
         self.db = db
         return
 
-
-class Work (api.Work, Record):
-
-    def register (self, db):
-        self._register (db)
-        db._op.execute ("INSERT INTO work (id) values (%d)" % self.id)
-        return
-
-
-class Expression (api.Expression, Record):
+class Person (Type, api.Person):
 
     def register (self, db):
+        ''' Register the attribute in the database '''
         self._register (db)
-        db._op.execute ("INSERT INTO expression (id) values (%d)" % self.id)
-        return
 
+        val = [self.id] + self.name 
+        db._op.execute ("INSERT INTO person_t (id, last, middle, first) "
+                        "values (%s, %s, %s, %s)", val)
+        db._db.commit ()
+        return self
     
-class Manifestation (api.Manifestation, Record):
-
-    def register (self, db):
-        self._register (db)
-        db._op.execute ("INSERT INTO manifestation (id) values (%d)" % self.id)
-        return
-
-    
-class Item (api.Item, Record):
-
-    def register (self, db):
-        self._register (db)
-        db._op.execute ("INSERT INTO item (id) values (%d)" % self.id)
-        return
     
 db = Database ("test", create = 0)
 
 Work ().register (db)
+
+p = Person ()
+p.name [p.FIRST] = 'Fred'
+p.register (db)
+
 
